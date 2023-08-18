3ccff977dddd414f65b79263c0e8d7f63fa63d07
artem <email.@mail.com>
2023-08-18 07:27:42 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿var cookiesRecipesApp = new CookiesRecipesApp;
+cookiesRecipesApp.Run();
+
+
+public class CookiesRecipesApp
+{
+    private readonly RecipesRepository _recipesRepository;
+    private readonly RecipesUserInteraction _recipesUserInteraction;
+
+    public CookiesRecipesApp(
+        RecipesRepository recipesRepository,
+        RecipesUserInteraction recipesUserInteraction)
+    {
+        _recipesRepository = recipesRepository;
+        _recipesUserInteraction = recipesUserInteraction;
+    }
+
+    public void Run()
+    {
+        var allRecipes = _recipesRepository.Read(filePath);
+        _recipesUserInteraction.PrintExistingRecipes(allRecipes);
+
+        _recipesUserInteraction.PromptToCreateRecipe();
+
+        var ingredients = _recipesUserInteraction.ReadIngredientsFromUser;
+
+        if (ingredients.Count > 0)
+        {
+            var recipe = new Recipe(ingredients);
+            allRecipes.Add(recipe);
+            _recipesRepository.Write(filePath, allRecipes);
+
+            _recipesUserInteraction.ShowMessage($"Recipe added: {recipe.ToString()}");
+            // _recipesUserInteraction.ShowMessage(recipe.ToString());
+        }
+        else
+        {
+            _recipesUserInteraction.ShowMessage(
+                "No ingredients have been selected. Recipe will not be saved");
+        }
+
+        _recipesUserInteraction.Exit();
+    }
+}
+
+public class RecipesUserInteraction
+{
+}
+
+public class RecipesRepository
+{
+}
